37c6328bc4686bba52b3c2bdf645a38b94e5d9d1
FlowOfASun <person1494@gmail.com>
2022-09-25 02:28:07 -0500

    part 2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, request, render_template, redirect
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:drowssap@localhost:5432/blogly'
@@ -37,7 +37,8 @@ def add_user():
 @app.route('/users/<int:uid>')
 def user_view(uid):
     u = User.query.get(uid)
-    return render_template('user.html', u = u)
+    posts = u.posts
+    return render_template('user.html', u = u, posts = posts)
 
 @app.route('/users/<int:uid>/edit')
 def user_edit(uid):
@@ -48,10 +49,13 @@ def user_update(uid):
     fname = request.form.get('fname')
     lname = request.form.get('lname')
     img_url = request.form.get('img_url')
+
     u = User.query.get(uid)
+    
     u.first_name = fname
     u.last_name = lname
     u.img_url = img_url
+
     db.session.add(u)
     db.session.commit()
     return redirect('/users')
@@ -62,4 +66,49 @@ def user_delete(uid):
     db.session.delete(u)
     db.session.commit()
     return redirect('/users')
+
+@app.route('/users/<int:uid>/posts/new')
+def new_post(uid):
+    return render_template('newpost.html', uid=uid)
+
+
+@app.route('/users/<int:uid>/posts/new', methods=["POST"])
+def make_post(uid):
+    title = request.form.get('title')
+    content = request.form.get('content')
+    p = Post(title = title, content = content, uid = uid)
+    db.session.add(p)
+    db.session.commit()
+    return redirect(f'/users/{uid}')
+
+@app.route('/posts/<int:pid>')
+def post_view(pid):
+    p = Post.query.get(pid)
+    return render_template('post.html', post = p)
+
+@app.route('/posts/<int:pid>/edit')
+def post_edit(pid):
+    p = Post.query.get(pid)
+    return render_template('editpost.html')
+
+@app.route('/posts/<int:pid>/edit', methods=["POST"])
+def post_update(pid):
+    title = request.form.get('title')
+    content = request.form.get('content')
+    p = Post.query.get(pid)
+    p.title = title
+    p.content = content
+    db.session.add(p)
+    db.session.commit()
+    return redirect(f'/users/{p.user.id}')
+
+@app.route('/posts/<int:pid>/delete', methods=["POST"])
+def post_delete(pid):
+    p = Post.query.get(pid)
+    print(p.user.id)
+    db.session.delete(p)
+    db.session.commit()
+    return redirect (f'/users/{p.user.id}')
+
+
 
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -21,3 +22,15 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     img_url = db.Column(db.Text, default=20)
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
+class Post(db.Model):
+    """Post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    uid = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
